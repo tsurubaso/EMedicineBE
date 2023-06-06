d6d258d7a28c99c79f1a5979a6caff3e95405d68
tsurubaso <tsurubaso@gmail.com>
2023-06-06 17:34:47 +0900

    Add project files.

---
+++ Models/DAL.cs
@@ -0,0 +1,141 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace EMedicineBE.Models
+{
+    public class DAL
+    {
+
+
+        public Response register (Users users, SqlConnection connection)
+        {
+
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("sp_register", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", users.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", users.LastName);
+            cmd.Parameters.AddWithValue("@Password", users.Password);
+            cmd.Parameters.AddWithValue("@Email", users.Email);
+            cmd.Parameters.AddWithValue("@Fund", 0);
+            cmd.Parameters.AddWithValue("@Type", "Users");
+            cmd.Parameters.AddWithValue("@Type", "Pending ");
+            connection.Open();
+            int i = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (i > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "User Registered Successful";
+
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User Registration failed";
+
+
+            }
+
+
+            return response;
+        }
+
+        public Response login(Users users, SqlConnection connection)
+        {
+
+            SqlDataAdapter da = new SqlDataAdapter("sp_login", connection);
+            da.SelectCommand.CommandType= CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@Email", users.Email);
+            da.SelectCommand.Parameters.AddWithValue("@Password", users.Password);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count> 0)
+            {
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                response.StatusCode = 200;
+                response.StatusMessage = "User is Valid";
+                response.user = user;
+
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User is invalid";
+                response.user = null;
+
+
+            }
+            return response;
+
+
+
+
+        }
+
+        public Response viewUser(Users users, SqlConnection connection)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("sp_viewUser", connection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@ID", users.ID);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            Users user = new Users();
+            if (dt.Rows.Count > 0)
+            {
+
+                user.ID = Convert.ToInt32(dt.Rows[0]["ID"]);
+                user.FirstName = Convert.ToString(dt.Rows[0]["FirstName"]);
+                user.LastName = Convert.ToString(dt.Rows[0]["LastName"]);
+                user.Email = Convert.ToString(dt.Rows[0]["Email"]);
+                user.Type = Convert.ToString(dt.Rows[0]["Type"]);
+                user.Fund = Convert.ToDecimal(dt.Rows[0]["Fund"]);
+                user.CreateOn = Convert.ToDateTime(dt.Rows[0]["CreateOn"]);
+                user.Password = Convert.ToString(dt.Rows[0]["Password"]);
+
+                response.StatusCode = 200;
+                response.StatusMessage = "User  exists";
+                response.user = user;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "User does not exist";
+
+
+            }
+            return response;
+        }
+
+        public Response updateprofile(Users users, SqlConnection connection)
+        {
+            Response response = new Response();
+            SqlCommand cmd = new SqlCommand("sp_updateProfile", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", users.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", users.LastName);
+            cmd.Parameters.AddWithValue("@Password", users.Password);
+            cmd.Parameters.AddWithValue("@Email", users.Email);
+            cmd.Parameters.AddWithValue("@Fund", 0);
+            cmd.Parameters.AddWithValue("@Type", "Users");
+            cmd.Parameters.AddWithValue("@Type", "Pending ");
+            //une heure et trois minutes
+
+
+
+        }
+
+
+
+
+
+    }
+}
+ 
